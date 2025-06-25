4cf04dc977ec623e246915d13f6567a6222a33b6
JrJaphet <juniorlesu9@gmail.com>
2025-06-25 10:58:33 +0300

    Remove redundant code from JaphyText widget

--- lib/widgets/japhy_text.dart
+++ lib/widgets/japhy_text.dart
@@ -32,37 +32,3 @@ class JaphyText extends StatelessWidget {
     );
   }
 }
-import 'package:flutter/material.dart';
-
-class JaphyText extends StatelessWidget {
-  const JaphyText({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ShaderMask(
-        shaderCallback: (bounds) => const LinearGradient(
-          colors: [Color(0xFF0D47A1), Colors.white, Colors.black],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ).createShader(bounds),
-        child: const Text(
-          'Japhy',
-          style: TextStyle(
-            fontSize: 80,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.white,
-            shadows: [
-              Shadow(
-                offset: Offset(3, 3),
-                blurRadius: 6,
-                color: Colors.black54,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
